1f961e9ccce4bf50e46a66e5b88b552b1f5771a9
Denar Padilla <denarpadilla.lionel@gmail.com>
2022-09-09 18:43:06 -0300

    agregando cosas al main y el cartWidget al navBar

--- src/components/main/ItemListContainer.jsx
+++ src/components/main/ItemListContainer.jsx
@@ -35,7 +35,6 @@ function ItemListContainer(props) {
           <img src="https://olcovers2.blob.core.windows.net/coverswp/2019/01/Bayesian-Methods-statistical-analysis_OpenLibra-300x427.jpg" alt=""  className="w-75 mx-auto d-block"/>
           <ItemCount stock={15} initial={0} onAdd={onAdd}/>
         </div> */}
-        <br />
       </div>
     );
   }
